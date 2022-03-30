708c78c9b1059086e8ed4f600cd970cd086438d9
mithin888 <mithingthomas@gmail.com>
2022-03-30 09:50:02 -0400

    implement basic routes and pages

---
+++ app.js
@@ -0,0 +1,51 @@
+// initializing node modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// initializing custom modules
+const content = require(__dirname + "/template.js");
+
+// using all modules
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+// express-specific methods
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// data
+const postArray = [];
+
+// get routes
+app.get("/", urlencodedParser, (req, res) => {
+  res.render("home", { homeContent: content.homeStartingContent, postContent: postArray });
+});
+
+app.get("/about", urlencodedParser, (req, res) => {
+  res.render("about", { aboutContent: content.aboutStartingContent });
+});
+
+app.get("/contact", urlencodedParser, (req, res) => {
+  res.render("contact", { contactContent: content.contactStartingContent });
+});
+
+app.get("/compose", urlencodedParser, (req, res) => {
+  res.render("compose");
+});
+
+
+// post routes
+app.post("/compose", urlencodedParser, (req, res) => {
+  let newPost = {
+    title: req.body.newTitle,
+    post: req.body.newPost
+  };
+  postArray.push(newPost);
+  res.redirect("/");
+});
+
+
+// server 
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
